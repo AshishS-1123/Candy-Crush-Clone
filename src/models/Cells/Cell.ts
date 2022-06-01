bd50628202b02d1ccd6df1d5d4180d39af373fc2
Ashish Shevale <shevaleashish@gmail.com>
2022-06-01 10:56:34 +0530

    Cell class to represent each cell of game board

---
+++ src/models/Cells/Cell.ts
@@ -0,0 +1,17 @@
+import { CellItem } from "../CellItems/CellItem";
+
+export interface Cell {
+    // Represents a cell from the board.
+    cellItem: CellItem;
+
+    // Whether the cell should be rendered on the canvas.
+    isVisible: boolean;
+
+    // method to call when destroying the cell item.
+    // implement score calculating here. But dont actually delete items here.
+    destroyCellItem: () => void;
+
+    // use this method in GravityHandler for swapping the current element with another.
+    // replaces the current item with given item and returns old item.
+    replaceCellItem: (newItem: CellItem) => CellItem;
+}
