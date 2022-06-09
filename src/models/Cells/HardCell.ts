b6239ce966de687ec956614efa94e03137519d1d
Ashish Shevale <shevaleashish@gmail.com>
2022-06-09 20:42:13 +0530

    Implement class to represent Hard Candies

---
+++ src/models/Cells/HardCell.ts
@@ -0,0 +1,19 @@
+import { CellType, Colors } from '~/setup';
+import { Cell } from './Cell';
+import { Global } from '~/EventBus';
+
+export class HardCell implements Cell {
+    cellType: CellType;
+    colorType: Colors;
+    image: HTMLImageElement;
+    isVisible: boolean;
+
+    constructor(color: Colors) {
+        this.cellType = 'HARD';
+        this.colorType = color;
+        this.isVisible = true;
+
+        // this.image.src = assignImageFromColor(color);
+        this.image = Global.spritePool.getHardCandy(color);
+    }
+}
